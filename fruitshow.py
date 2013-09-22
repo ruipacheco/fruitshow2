8cbea043345db221da8336ad7fd30e70eae5b9ec
Rui Pacheco <rui.pacheco@gmail.com>
2013-09-23 01:20:59 +0200

    Did it go in now?

--- fruitshow.py
+++ fruitshow.py
@@ -1,2 +1,10 @@
-from app import app
-app.run(debug = True)
+from flask_failsafe import failsafe
+
+@failsafe
+def create_app():
+    from app import app
+    app.config.from_object('config')
+    app.run()
+    
+if __name__ == "__main__":
+    create_app().run()
