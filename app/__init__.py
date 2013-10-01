845571281501a6ae5e5451faad78f2be7803a907
Rui Pacheco <rui.pacheco@gmail.com>
2013-10-01 23:57:41 +0200

    Embed youtube url's in page.

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,17 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.misaka import Misaka
+from micawber.providers import bootstrap_basic
+from micawber.contrib.mcflask import add_oembed_filters
 
 app = Flask(__name__)
+
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 Misaka(app)
+
+oembed_providers = bootstrap_basic()
+add_oembed_filters(app, oembed_providers)
 
 from app import views, models, forms
