63f070cb1fa013392c9c4b85183642044ed4e5b0
Rui Pacheco <rui.pacheco@gmail.com>
2013-10-01 17:53:40 +0200

    Markdown

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask.ext.markdown import Markdown
 
 app = Flask(__name__)
+Markdown(app)
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
