1f56e8bae0163d80b09ced2e0d5d6f41693feddb
Rui Pacheco <rui.pacheco@gmail.com>
2013-10-01 17:58:21 +0200

    Replaced markdown with Misaka - faster

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from flask.ext.markdown import Markdown
+from flask.ext.misaka import Misaka
 
 app = Flask(__name__)
-Markdown(app)
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
+Misaka(app)
 
 from app import views, models, forms
