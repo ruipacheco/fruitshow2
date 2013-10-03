0f948de0efd3b333ba3cb34391f0269a428ea226
Rui Pacheco <rui.pacheco@gmail.com>
2013-10-03 23:17:39 +0200

    Added gravatar support

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.misaka import Misaka
 from flask.ext.mail import Mail
+from flask.ext.gravatar import Gravatar
 from micawber.providers import bootstrap_basic
 from micawber.contrib.mcflask import add_oembed_filters
 
@@ -18,5 +19,13 @@ login_manager.login_view = 'login'
 Misaka(app)
 oembed_providers = bootstrap_basic()
 add_oembed_filters(app, oembed_providers)
+
+gravatar = Gravatar(app,
+                    size=50,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    use_ssl=False,
+                    base_url=None)
 
 from app import views, models, forms
